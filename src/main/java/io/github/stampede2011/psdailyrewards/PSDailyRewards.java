3857b81b7eee69e0ff29ca76ab69ec237a9252be
Stampede2011 <carterblowers01@gmail.com>
2021-07-22 19:45:08 -0500

    version

--- src/main/java/io/github/stampede2011/psdailyrewards/PSDailyRewards.java
+++ src/main/java/io/github/stampede2011/psdailyrewards/PSDailyRewards.java
@@ -43,7 +43,7 @@ public class PSDailyRewards {
     public static final String NAME = "PSDailyRewards";
     public static final String AUTHORS = "Stampede2011";
     public static final String DESCRIPTION = "Daily rewards plugin for the PokeSkies Network!";
-    public static final String VERSION = "1.12.2-1.0.0";
+    public static final String VERSION = "1.12.2-1.0.1";
 
     @Inject
     private Logger logger;
